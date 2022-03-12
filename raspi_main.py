cc50bc57d1237a97b26f67b531fd2c16a8ccbcfc
anish chaudhary <kingpin@pop-os.localdomain>
2022-03-12 18:55:19 +0530

    init

---
+++ raspi_main.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+import RPi.GPIO as GPIO
+import numpy as np
+import tflite_runtime.interpreter as tflite
+import cv2
+
+model_path="model.tflite"
+led = 21
+p = 0
+beta = 0.15
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(led, GPIO.OUT)
+
+interpreter = tflite.Interpreter(model_path=model_path)
+interpreter.allocate_tensors()
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+input_shape = input_details[0]['shape']
+
+vid = cv2.VideoCapture(0)
+
+while True:
+	ret,frame = vid.read()
+	input_data = np.array([cv2.resize(frame,(320,320))],dtype=np.ubyte)
+	interpreter.set_tensor(input_details[0]['index'], input_data)
+	interpreter.invoke()
+	classes = interpreter.get_tensor(output_details[1]['index'])
+	probablities = interpreter.get_tensor(output_details[2]['index'])
+	if classes[0][0] == 0.0 and probablities[0][0] >= 0.5:
+		p = beta*p + (1-beta)*1
+	else:
+		p = beta*p + (1-beta)*0
+	if p >= 0.5:
+		#print('on')
+        GPIO.output(led, GPIO.HIGH)
+	else:
+		#print('off')
+        GPIO.output(led, GPIO.LOW)
+
+cap.release()
